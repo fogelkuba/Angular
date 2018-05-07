10c968823ccedb61d9e6e0c09b3dfa3a4683e524
fogelkuba <fogelkuba@gmail.com>
2018-05-07 23:35:09 +0200

    fix

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,9 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  template: ''
-});
+  templateUrl: './server.component.html',
+  // styleUrls: ['./server.component.css']
+})
 
 export class ServerComponent{
 
