d8e2af5505bc84691bd4223eaeb5921ad88f4980
fogelkuba <fogelkuba@gmail.com>
2018-05-08 23:02:32 +0200

    fix

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]',
   template: '<h2>Serwery:</h2><app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
